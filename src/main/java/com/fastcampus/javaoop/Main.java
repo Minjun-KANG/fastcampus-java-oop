3d74fddca3be7a1dfa0c2aa4482bc1099fd15081
Minjun-KANG <inventer@kakao.com>
2022-03-14 21:08:44 +0900

    init commit

--- src/main/java/com/fastcampus/javaoop/Main.java
+++ src/main/java/com/fastcampus/javaoop/Main.java
@@ -1,12 +1,17 @@
 package com.fastcampus.javaoop;
 
 import com.fastcampus.javaoop.logic.BubbleSort;
+import com.fastcampus.javaoop.logic.JavaSort;
+import com.fastcampus.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        //구현체 중에 BubbleSort 를 사용할 것을 명확하게 알고 있으니, 현재는 Strongly coupled 상태임.
+        //이를 다시 루즐리 커플드로 바꿈.
+        //BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
 
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
--- src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
+++ src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class JavaSort <T extends Comparable<T>>{
+public class JavaSort <T extends Comparable<T>> implements Sort<T>{
 
     public List<T> sort(List<T> list) {
         List<T> output = new ArrayList<>(list);
